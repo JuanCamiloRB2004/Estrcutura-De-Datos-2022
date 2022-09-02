9f1ecfddc016e24e128d9efc2551dc05f7370edb
JuanCamiloRB2004 <juanc.ramirezb@autonoma.edu.co>
2022-09-02 15:54:40 -0500

    gitignore and activity1 files 1.0

--- activity1.py
+++ activity1.py
@@ -13,15 +13,15 @@ class Activity1:
         while True:
             try:
                 cantidad_paises = int(input('ingrese numero de paises: '))
-                for contador in range(1, contador):
-                    nombre_pais = ('ingrese nombre de pais: ')
+                for contador in range(1, cantidad_paises+1):
+                    nombre_pais = input('ingrese nombre de pais: ')
                     while True:
                         try:
                             poblacion_paises = int(input('ingrese poblacion del pais: '))
                             break
                         except:
                             print('ingrese valor numerico')
-                    cantidad_paises.append(contador, nombre_pais, poblacion_paises)
+                        lista_paises.append((contador, nombre_pais, poblacion_paises))
                 break    
             except:
                 print('por favor, ingrese un valor numerico')
