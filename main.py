0ed7f18362a55eca360799643977ef15dddc9a82
JuanCamiloRB2004 <juanc.ramirezb@autonoma.edu.co>
2022-09-02 15:23:53 -0500

    activity1.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('hola')
+'''
+    author: Juan camilo ramirez buritica
+    verssion: 1.0
+    lenguage: python
+'''
+#importamos clase desde el archivo
+from activity1 import Activity1
+
+#se crea instancia
+inst_activity1 = Activity1
+#inst_activity1.poblacion_pais()
+inst_activity1.show_info()
